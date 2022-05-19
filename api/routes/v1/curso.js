87d65fa2021a50a9f8b67c11df73c67d72a3dbfb
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-05-19 11:00:20 -0300

    instalação do body parser

--- api/routes/v1/curso.js
+++ api/routes/v1/curso.js
@@ -1,6 +1,15 @@
 module.exports = (routerApp) => {
   routerApp.get("/cursos", (req, res, next) => {
-    console.log(req.query);
+    console.log(req.query.status);
+
+    // const status = req.query.status;
+
+    // if (status) {
+    //   if (!status.includes(req.query.status));
+    //   return res.status(400).send("O status informado é inválido");
+    // }
+
+    // res.status(200).json(data);
 
     res.status(200).json("Rota cursos funcionando!");
   });
